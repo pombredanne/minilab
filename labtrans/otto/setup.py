a5eee9e5beeb6016663ae921d49792ce0058b699
Ivan Ogassawara <ivan.ogassawara@gmail.com>
2014-01-14 15:33:47 -0200

    ajustes para melhorar leitura

--- labtrans/otto/setup.py
+++ labtrans/otto/setup.py
@@ -4,8 +4,41 @@
 """
 from __future__ import division, print_function, unicode_literals
 from conn import db_connect, db_cursor
-from copy import deepcopy
-import settings
+
+settings_template = '''# -*- coding: utf-8 -*-
+"""
+Local Settings file
+
+"""
+DEBUG = True
+
+_PATH_ROOT = __file__[:-len(__name__) + 2]
+
+DEFAULT_FROM_EMAIL = 'ivan.ogassawara@labtrans.ufsc.br'
+
+DATABASE = {
+        'ENGINE':   'postgresql',
+        'NAME':     'lab_temperature',
+        'USER':     '***',
+        'PASSWORD': '***',
+        'HOST':     '***',
+        'PORT':     '5432'
+}
+
+email = {
+    'to': 'ivan.ogassawara@gmail.com',
+    'from': 'ivan.ogassawara@labtrans.ufsc.br',
+    'smtp-server': 'smtp.labtrans.ufsc.br'
+}
+'''
+
+try:
+    import settings
+except:
+    with open('settings.py', 'w') as f:
+        f.write(settings_template)
+        print('Configure o arquivo settings.py e tente executar novamente.')
+        exit()
 
 db_name = settings.DATABASE['NAME']
 settings.DATABASE['NAME'] = 'postgres'
