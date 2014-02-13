f687bed261e60215ec290aa40f50feda0f1545f0
ivan <ivan.ogassawara@gmail.com>
2014-02-13 17:32:46 -0200

    multiprocessing simulation

--- gui/plotter/multi_chart.py
+++ gui/plotter/multi_chart.py
@@ -4,42 +4,71 @@ from matplotlib.ticker import EngFormatter
 
 import numpy as np
 
-plt.ion()
-plt.show()
 
-data_size = 100
-time = np.arange(0, 1, 1/data_size)
-formatter = EngFormatter(unit='s', places=1)
-# interval
-a = -8
-b = +8
-frame_limits = [0, 1, -10, 10]
-while True:
-    plt.clf()
+def start():
+    plt.ion()
+    plt.show()
 
-    for _ in range(5):
-        ax = plt.subplot(311)
-        ax.xaxis.set_major_formatter(formatter)
-        ax.axis(frame_limits)
-        data = (b - a) * np.random.random_sample((data_size,)) + a
-        ax.grid()
-        ax.plot(time, data)
 
-    for _ in range(5):
-        ax = plt.subplot(312)
-        ax.xaxis.set_major_formatter(formatter)
-        ax.axis(frame_limits)
-        ax.grid()
-        data = (b - a) * np.random.random_sample((data_size,)) + a
-        ax.plot(time, data)
+def stop():
+    plt.ioff()
 
-    for _ in range(5):
-        ax = plt.subplot(313)
-        ax.xaxis.set_major_formatter(formatter)
-        ax.axis(frame_limits)
-        ax.grid()
-        data = (b - a) * np.random.random_sample((data_size,)) + a
-        ax.plot(time, data)
 
-    plt.draw()
-    plt.pause(0.00001)
+def plotter(data_size=100, wait=0.00001):
+    start()
+
+    time = np.arange(0, 1, 1/data_size)
+    formatter = EngFormatter(unit='s', places=1)
+    # interval
+    frame_limits = [0, 1, -10, 10]
+
+    while True:
+        data = (yield)
+
+        chart_n = len(data.keys())
+
+        if not chart_n:
+            continue
+
+        plt.clf()
+
+        i = 0
+        for name in data:
+            i += 1
+            chart_id = chart_n*100 + 10 + i
+            for ch in data[name]:
+                ax = plt.subplot(chart_id)
+                ax.xaxis.set_major_formatter(formatter)
+                ax.axis(frame_limits)
+                ax.grid()
+                ax.set_xlabel(name)
+                ax.plot(time, data[name][ch])
+
+        plt.draw()
+        plt.pause(wait)
+
+
+if __name__ == '__main__':
+    def main():
+        data_size = 100
+        mplt = plotter(data_size=data_size, wait=0.00001)
+        mplt.next()
+
+        a = -8
+        b = +8
+
+        while True:
+            data = {
+                'Dev1': (b - a) * np.random.random_sample((data_size,)) + a,
+                'Dev2': (b - a) * np.random.random_sample((data_size,)) + a,
+                'Dev3': (b - a) * np.random.random_sample((data_size,)) + a
+            }
+            mplt.send(data)
+
+        stop()
+
+    try:
+        main()
+    except:
+        stop()
+        print('Exit.')
--- paralelo/multi_process/daq.py
+++ paralelo/multi_process/daq.py
@@ -1,40 +1,98 @@
-from __future__ import generators
-import sys, time
+from __future__ import generators, print_function
+from multiprocessing import Pool
+from collections import defaultdict
 
-threads = []
-TOTALSWITCHES = 10**2
-NUMTHREADS    = 10**1
+import sys
+import time
+import os
+import numpy as np
+import psutil
+from copy import deepcopy
+# internal
+#sys.path.append('../../..')
+from gui.plotter.multi_chart import plotter
 
-def null_factory(i):
-    def empty():
-        while 1:
-            yield None
-            print i
-    return empty()
 
-def quitter():
-    for n in xrange(TOTALSWITCHES/NUMTHREADS):
-        yield None
-        print 'q'
+def memory_usage():
+    """
+    Return memory usage in MB unity.
+    """
+    process = psutil.Process(os.getpid())
+    return process.get_memory_info()[0]/float(2**20)
 
-def scheduler():
-    global threads
-    T = None
-    try:
-        while 1:
-            for thread in threads:
-                T = thread
-                thread.next()
-    except StopIteration:
-        print thread
-        pass
+
+class BufferRing(object):
+    """
+
+    """
+    data_buffer = defaultdict(dict)
+
+    def append(self, data):
+        """
+        Append data to buffer
+
+        """
+        for buffer_name in data:
+            for ch in data[buffer_name]:
+                if not ch in self.data_buffer[buffer_name]:
+                    self.data_buffer[buffer_name][ch] = []
+                self.data_buffer[buffer_name][ch] += data[buffer_name][ch]
+
+    def extract(self, callback=None):
+        """
+        Extract data from buffer
+
+        """
+        data = defaultdict(dict)
+
+        for name in self.data_buffer:
+            for ch in self.data_buffer[name]:
+                if len(self.data_buffer[name][ch]) < 1000:
+                    data[name][ch] = [0] * 1000
+                else:
+                    data[name][ch] = self.data_buffer[name][ch][:1000]
+                    self.data_buffer[name][ch][:] = (
+                        self.data_buffer[name][ch][1000:]
+                    )
+        return data
+
+
+def daq(*args):
+    """
+
+    """
+    dev = args[0]
+
+    result = {}
+    for i in xrange(32):
+        result[i] = list(np.random.random(1000))
+
+    return dev, result
+
 
 if __name__ == "__main__":
-    for i in range(NUMTHREADS):
-        threads.append(null_factory(i))
-    threads.append(quitter())
-    starttime = time.clock()
-    scheduler()
-    print "TOTAL TIME:    ", time.clock()-starttime
-    print "TOTAL SWITCHES:", TOTALSWITCHES
-    print "TOTAL THREADS: ", NUMTHREADS
+    threads = []
+
+    bf = BufferRing()
+
+    threads.append('Dev1')
+    threads.append('Dev2')
+    threads.append('Dev4')
+    threads.append('Dev5')
+
+    mplt = plotter(data_size=1000)
+    mplt.next()
+
+    pool = Pool(processes=4)  # start 4 worker processes
+    pool2 = Pool(processes=1)  # start 4 worker processes
+
+    while True:
+        if memory_usage() > 1280:
+            raise Exception('Memory usage exceed 1GB.')
+
+        start_time = time.time()
+        data = dict(pool.map(daq, threads))
+        print('%f' % (time.time() - start_time), end=' - ')
+        bf.append(data)
+        mplt.send(bf.extract())
+        print('%f' % (time.time() - start_time))
